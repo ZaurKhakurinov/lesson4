fa1dbb98597ac6effd33ea96e4fd14e5464d1825
ZaurKhakurinov <lazter@mail.ru>
2022-12-24 13:32:28 +0300

    add homework

--- Case29/Program.cs
+++ Case29/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = GetArray(8);
+Console.Write($"[{String.Join(",", array)}]");
+
+int[] GetArray(int size)
+{
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(999);
+    }
+
+    return result;
+}
