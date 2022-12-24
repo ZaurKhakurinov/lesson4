6d6b2828965bdc0607ef1998df8ed64d82ecf5da
ZaurKhakurinov <lazter@mail.ru>
2022-12-24 11:21:50 +0300

    add work on the lesson

--- Case28/Program.cs
+++ Case28/Program.cs
@@ -1,4 +1,4 @@
-﻿// напишите прграмму которая принимает на вход число А и выдает сумму чисел от 1 до А
+﻿// принимает числа А и выдает произведение от 1 до А
 Console.Clear();
 Console.WriteLine("Ввведидите число: ");
 int limit = int.Parse(Console.ReadLine());
@@ -6,8 +6,8 @@ Console.WriteLine ($"{limit}->{GetSum (limit)}");
 
 int GetSum (int limit)
 {
-    int sum=0;
+    int sum=1;
     for (int i = 1; i < limit; i++)    
-        sum += i;
+        sum *= i;
     return sum;
 }
