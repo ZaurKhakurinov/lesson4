7feda2e4300d2c213e16b522c6faebaf14fc6fc3
ZaurKhakurinov <lazter@mail.ru>
2022-12-24 10:53:55 +0300

    add work on the lesson

---
+++ Case24/Program.cs
@@ -0,0 +1,15 @@
+﻿// напишите прграмму которая принимает на вход число А и выдает сумму чисел от 1 до А
+Console.Clear();
+Console.WriteLine("Ввведидите число: ");
+int limit = int.Parse(Console.ReadLine());
+Console.WriteLine ($"{limit}->{GetSum (limit)}");
+
+int GetSum (int limit)
+{
+    int sum=0;
+    for (int i = 1; i < limit; i++);    
+        sum += i;
+    return sum;
+}
+
+//  не работает
