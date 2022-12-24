224eb0c12c84533e36db3f6ad97c44acef32f034
ZaurKhakurinov <lazter@mail.ru>
2022-12-24 11:06:15 +0300

    add work on the lesson

---
+++ Case26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдает колическтво цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Ввведидите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine ($"Количество цифр = {GetCountNums (num)}");
+
+int GetCountNums(int number)
+{
+    int count = 0;
+    while (number>0)
+    {
+        count++;
+        number/=10;
+    }
+    return count;
+}
